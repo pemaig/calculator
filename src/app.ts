a7bc36bafe4775c52efa7a5b545bff0d8f3b123b
Maxim Pestriakov <maxim.pestriakov@sirenltd.com>
2024-01-20 13:55:53 +0200

    improve architecture with OOP approach

--- src/app.ts
+++ src/app.ts
@@ -1,139 +1,149 @@
-import * as Calculator from './calculator.js'
-import * as Elements from './elements.js'
-import { type Operators } from './types.js'
+import { clearBtn, display, numsButtons, operationButtons, resultBtn } from './elements.js'
+import { Calculator } from './calculator.js'
 
 /**
  * PROBLEMS:
  * + chaining operations: val + operation + val + operation + result
  * + error handling
  * + dot sign
+ * - make OOP with design patterns in mind
  * - rename project to serious_calculator
  * - add tests
  * - cases with Infinity/NaN: division by 0;
  * - could be too many symbols, need to set boundary
- * - make OOP with design patterns in mind
  * - add vite
  * - refactor styles with css grid
  * - add keyboard support
  */
+//
+// function clearBtnOnClick (_: Event): void {
+//     Calculator.setDisplayedValue(0)
+//     Calculator.setPreviousValue(0)
+//     Calculator.setCurrentOperation('=')
+//     Calculator.setInsertMode(false)
+// }
+//
+// function numsButtonsOnClick (e: Event): void {
+//     const input = (e.target as HTMLElement).innerText
+//     const displayedValue = Calculator.getDisplayedValue()
+//     const isInsertMode = Calculator.getInsertMode()
+//     const isAlreadyWithDot = displayedValue.includes('.')
+//     const isDotInput = input === '.'
+//     let newDisplayedValue: string | number = input
+//    
+//     function numsInputStrategy (): void {
+//         if (isInsertMode) {
+//             // use Number to avoid leading zero
+//             newDisplayedValue = Number(`${displayedValue}${input}`)
+//         }
+//     }
+//    
+//     function dotInputStrategy (): void {
+//         if (isInsertMode && isAlreadyWithDot) { return }
+//        
+//         if (isInsertMode) {
+//             newDisplayedValue = `${displayedValue}${input}`
+//         } else {
+//             newDisplayedValue = `0${input}`
+//         }
+//     }
+//    
+//     if (isDotInput) {
+//         dotInputStrategy()
+//     } else {
+//         numsInputStrategy()
+//     }
+//    
+//     if (!isInsertMode) {
+//         Calculator.setInsertMode(true)
+//     }
+//    
+//     Calculator.setDisplayedValue(newDisplayedValue)
+// }
+//
+// function isUnaryOperator (operator: string): boolean {
+//     return (
+//         operator === '+/-' ||
+//         operator === '%'
+//     )
+// }
+//
+// function operationBtnOnClick (e: Event): void {
+//     const newOperation = (e.target as HTMLElement).innerText as Operators
+//     let result: number
+//  
+//     if (isUnaryOperator(newOperation)) {
+//         Calculator.setCurrentOperation(newOperation)
+//         result = calculateResult()
+//         Calculator.setCurrentOperation('=')
+//     } else {
+//         result = calculateResult()
+//         Calculator.setCurrentOperation(newOperation)
+//     }
+//  
+//     Calculator.setPreviousValue(result)
+//     Calculator.setDisplayedValue(result)
+//     Calculator.setInsertMode(false)
+// }
+//
+// function calculateResult (): number {
+//     const leftOperand = Calculator.getPreviousValue()
+//     const rightOperand = Number(Calculator.getDisplayedValue())
+//     const currentOperation = Calculator.getCurrentOperation()
+//     let result = leftOperand
+//  
+//     switch (currentOperation) {
+//         case '+/-':
+//             result = 0 - rightOperand
+//             break
+//         case '%':
+//             result = rightOperand / 100
+//             break
+//         case '+':
+//             result += rightOperand
+//             break
+//         case '-':
+//             result -= rightOperand
+//             break
+//         case '*':
+//             result *= rightOperand
+//             break
+//         case '/':
+//             result /= rightOperand
+//             break
+//         case '=':
+//         default:
+//             result = rightOperand
+//     }
+//  
+//     return result
+// }
+//
+// function resultBtnOnClick (_: Event): void {
+//     const result = calculateResult()
+//
+//     Calculator.setDisplayedValue(result)
+//     Calculator.setCurrentOperation('=')
+//     Calculator.setInsertMode(false)
+// }
+//
+// function errorHandler (error: ErrorEvent): void {
+//     console.error(error)
+// }
+//
+// Elements.numsButtons.forEach((button) => { button.addEventListener('click', numsButtonsOnClick) })
+// Elements.operationButtons.forEach((button) => { button.addEventListener('click', operationBtnOnClick) })
+// Elements.clearBtn.addEventListener('click', clearBtnOnClick)
+// Elements.resultBtn.addEventListener('click', resultBtnOnClick)
+// window.addEventListener('error', errorHandler)
 
-function clearBtnOnClick (_: Event): void {
-    Calculator.setDisplayedValue(0)
-    Calculator.setPreviousValue(0)
-    Calculator.setCurrentOperation('=')
-    Calculator.setInsertMode(false)
-}
+const calculator = Calculator.getInstance(display)
 
-function numsButtonsOnClick (e: Event): void {
-    const input = (e.target as HTMLElement).innerText
-    const displayedValue = Calculator.getDisplayedValue()
-    const isInsertMode = Calculator.getInsertMode()
-    const isAlreadyWithDot = displayedValue.includes('.')
-    const isDotInput = input === '.'
-    let newDisplayedValue: string | number = input
-    
-    function numsInputStrategy (): void {
-        if (isInsertMode) {
-            // use Number to avoid leading zero
-            newDisplayedValue = Number(`${displayedValue}${input}`)
-        }
-    }
-    
-    function dotInputStrategy (): void {
-        if (isInsertMode && isAlreadyWithDot) { return }
-        
-        if (isInsertMode) {
-            newDisplayedValue = `${displayedValue}${input}`
-        } else {
-            newDisplayedValue = `0${input}`
-        }
-    }
-    
-    if (isDotInput) {
-        dotInputStrategy()
-    } else {
-        numsInputStrategy()
-    }
-    
-    if (!isInsertMode) {
-        Calculator.setInsertMode(true)
-    }
-    
-    Calculator.setDisplayedValue(newDisplayedValue)
-}
-
-function isUnaryOperator (operator: string): boolean {
-    return (
-        operator === '+/-' ||
-        operator === '%'
-    )
-}
-
-function operationBtnOnClick (e: Event): void {
-    const newOperation = (e.target as HTMLElement).innerText as Operators
-    let result: number
-  
-    if (isUnaryOperator(newOperation)) {
-        Calculator.setCurrentOperation(newOperation)
-        result = calculateResult()
-        Calculator.setCurrentOperation('=')
-    } else {
-        result = calculateResult()
-        Calculator.setCurrentOperation(newOperation)
-    }
-  
-    Calculator.setPreviousValue(result)
-    Calculator.setDisplayedValue(result)
-    Calculator.setInsertMode(false)
-}
-
-function calculateResult (): number {
-    const leftOperand = Calculator.getPreviousValue()
-    const rightOperand = Number(Calculator.getDisplayedValue())
-    const currentOperation = Calculator.getCurrentOperation()
-    let result = leftOperand
-  
-    switch (currentOperation) {
-        case '+/-':
-            result = 0 - rightOperand
-            break
-        case '%':
-            result = rightOperand / 100
-            break
-        case '+':
-            result += rightOperand
-            break
-        case '-':
-            result -= rightOperand
-            break
-        case '*':
-            result *= rightOperand
-            break
-        case '/':
-            result /= rightOperand
-            break
-        case '=':
-        default:
-            result = rightOperand
-    }
-  
-    return result
-}
-
-function resultBtnOnClick (_: Event): void {
-    const result = calculateResult()
-
-    Calculator.setDisplayedValue(result)
-    Calculator.setCurrentOperation('=')
-    Calculator.setInsertMode(false)
-}
-
-function errorHandler (error: ErrorEvent): void {
-    console.error(error)
-}
-
-Elements.numsButtons.forEach((button) => { button.addEventListener('click', numsButtonsOnClick) })
-Elements.operationButtons.forEach((button) => { button.addEventListener('click', operationBtnOnClick) })
-Elements.clearBtn.addEventListener('click', clearBtnOnClick)
-Elements.resultBtn.addEventListener('click', resultBtnOnClick)
-window.addEventListener('error', errorHandler)
+numsButtons.forEach(btn => {
+    btn.addEventListener('click', () => { calculator.insertNumber((btn as HTMLElement).innerText) })
+})
+clearBtn.addEventListener('click', () => { calculator.clear() })
+operationButtons.forEach(btn => {
+    btn.addEventListener('click', () => { calculator.setOperation((btn as HTMLElement).innerText) })
+})
+resultBtn.addEventListener('click', () => { calculator.compute() })
--- src/calculator.ts
+++ src/calculator.ts
@@ -1,38 +1,108 @@
-import { display } from './elements.js'
 import { type Operators } from './types.js'
 
-let previousValue: number = 0
-let currentOperation: Operators = '='
-let isInsertMode: boolean = true
-
-export function getInsertMode (): boolean {
-    return isInsertMode
-}
-
-export function setInsertMode (val: boolean): void {
-    isInsertMode = val
-}
-
-export function getDisplayedValue (): string {
-    return display.innerText
-}
-
-export function setDisplayedValue (newVal: number | string): void {
-    display.innerText = String(newVal)
-}
-
-export function getPreviousValue (): number {
-    return previousValue
-}
-
-export function setPreviousValue (val: number): void {
-    previousValue = val
-}
-
-export function getCurrentOperation (): Operators {
-    return currentOperation
-}
-
-export function setCurrentOperation (newOperation: Operators): void {
-    currentOperation = newOperation
+export class Calculator {
+    private static _instance: Calculator
+    
+    private readonly _display: HTMLElement
+    private _currentValue!: number | string
+    private _previousValue!: number | string
+    private _currentOperation!: Operators
+    
+    static getInstance (displayElement: HTMLElement): Calculator {
+        if (Calculator._instance === undefined) {
+            Calculator._instance = new Calculator(displayElement)
+        }
+        
+        return Calculator._instance
+    }
+    
+    private constructor (displayElement: HTMLElement) {
+        this._display = displayElement
+        this.clear()
+    }
+    
+    private updateDisplay (showDefaultValue: boolean = false): void {
+        if (showDefaultValue) {
+            this._display.innerText = '0'
+            return
+        }
+        this._display.innerText = this._currentValue.toString()
+    }
+    
+    clear (): void {
+        this._currentValue = ''
+        this._previousValue = ''
+        this._currentOperation = '='
+        this.updateDisplay(true)
+    }
+    
+    insertNumber (value: string): void {
+        const isPeriod = value === '.'
+        const isCurrentValEmpty = this._currentValue.toString() === ''
+        const isCurrentValWithPeriod = this._currentValue.toString().includes('.')
+        let newValue = this._currentValue.toString() + value.toString()
+        
+        if (isPeriod && isCurrentValWithPeriod) {
+            return
+        }
+        if (isPeriod && isCurrentValEmpty) {
+            newValue = '0.'
+        }
+        
+        this._currentValue = newValue
+        this.updateDisplay()
+    }
+    
+    setOperation (operation: string): void {
+        const isUnary = operation === '+/-' || operation === '%'
+        
+        if (isUnary) {
+            this._currentOperation = operation as Operators
+            this.compute(true)
+        } else {
+            this.compute()
+            this._currentOperation = operation as Operators
+        }
+        this._previousValue = this._currentValue
+        this._currentValue = ''
+    }
+    
+    compute (isUnary: boolean = false): void {
+        let result = parseFloat(this._previousValue.toString())
+        const rightOperand = parseFloat(this._currentValue.toString())
+        
+        if (
+            !isUnary &&
+            (isNaN(result) || isNaN(rightOperand))
+        ) {
+            return
+        }
+        
+        switch (this._currentOperation) {
+            case '+/-':
+                result = 0 - rightOperand
+                break
+            case '%':
+                result = rightOperand / 100
+                break
+            case '+':
+                result += rightOperand
+                break
+            case '-':
+                result -= rightOperand
+                break
+            case '*':
+                result *= rightOperand
+                break
+            case '/':
+                result /= rightOperand
+                break
+            case '=':
+            default:
+                result = rightOperand
+        }
+        
+        this._currentValue = result
+        this.updateDisplay()
+    }
 }
